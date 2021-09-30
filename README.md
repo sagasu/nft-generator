2786f31b328dfc515f03385c0df839d85eb88ccf
Mateusz Kopij <sagasu@gmail.com>
2021-09-30 20:25:27 +0100

    running everything as nodemon

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # nft-generator
+# Running
+Best way, just run:  
+`npm run start-demon`  
+  
+This will monitor changes in index.mjs file as you code, and trace extra info if errors happen.
+
 node still doesn't support in an easy fashion ES modules natively (we would need to use `require` keyword instead of `import from`). That is why I am using experimental mode and running files as `mjs`. To run just fire `npm start`.
--- index.mjs
+++ index.mjs
@@ -1,13 +1,18 @@
 import fs from "fs";
 import can from "canvas";
 
-const canvas = can.createCanvas(1000, 1000);
+const height = 1000;
+const width = 1000;
+
+const canvas = can.createCanvas(height, width);
 const ctx = canvas.getContext("2d");
 
-const drawLayer = () => {
-    can.loadImage("./eye_ball.png").then(data => {
-        console.log(data);
-    });
+const drawLayer = async () => {
+    const x = 0;
+    const y = 0;
+    const image = await can.loadImage("./eye_ball.png");
+    ctx.drawImage(image, x, y, height, width);
+    console.log("done");
 };
 
 drawLayer();
