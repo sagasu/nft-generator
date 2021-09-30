dfb6c27e8fa99054ee5238eb048235fc9bd830ca
Mateusz Kopij <sagasu@gmail.com>
2021-09-30 21:57:26 +0100

    adding layers and config code to structure it.

--- index.mjs
+++ index.mjs
@@ -4,6 +4,10 @@ import can from "canvas";
 const height = 1000;
 const width = 1000;
 
+const saveLayer = (_canvas) => {
+    fs.writeFileSync("./newImage.png", _canvas.toBuffer("image/png"));
+};
+
 const canvas = can.createCanvas(height, width);
 const ctx = canvas.getContext("2d");
 
@@ -12,7 +16,7 @@ const drawLayer = async () => {
     const y = 0;
     const image = await can.loadImage("./eye_ball.png");
     ctx.drawImage(image, x, y, height, width);
-    console.log("done");
+    saveLayer(canvas);
 };
 
 drawLayer();
